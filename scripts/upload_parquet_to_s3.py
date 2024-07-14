c6b35ad5563c00a190b6e4b0eabea2086e430fc4
brunomcr <bruno.mcreal@gmailcom>
2024-07-14 12:50:09 -0300

    Update S3 upload feature: added session token parameter

--- scripts/upload_parquet_to_s3.py
+++ scripts/upload_parquet_to_s3.py
@@ -1,18 +1,10 @@
 import os
-
 from src.data_processing.data_scraper import scrape_b3_data
-from src.data_processing.data_uploader import upload_to_aws
+from src.data_processing.data_uploader import upload_to_s3
+
+
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
 
 if __name__ == "__main__":
     local_file = scrape_b3_data()
-    bucket_name = os.getenv('BUCKET_NAME')
-    s3_file_path = os.getenv('S3_FILE_PATH')
-    acess_key = os.getenv('ACESS_KEY')
-    secret_key = os.getenv('SECRET_KEY')
-
-    uploaded = upload_to_aws(local_file, bucket_name, s3_file_path, acess_key, secret_key)
-
-    if uploaded:
-        print("File uploaded successfully")
-    else:
-        print("File upload failed")
+    uploaded = upload_to_s3(local_file, AWS_BUCKET_NAME)
--- src/data_processing/data_uploader.py
+++ src/data_processing/data_uploader.py
@@ -1,19 +1,30 @@
+import os
 import boto3
+import datetime
 from botocore.exceptions import NoCredentialsError, ClientError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')
 
 
-def upload_to_aws(local_file, bucket, s3_file, acess_key, secret_key, object_name=None, prefix=None):
-
+def upload_to_s3(local_file, bucket, object_name=None, prefix=None):
     session = boto3.Session(
-        aws_access_key_id=acess_key,
-        aws_secret_access_key=secret_key
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        aws_session_token=AWS_SESSION_TOKEN
     )
 
     s3 = session.client('s3')
 
+    s3_file_path = f'B3/{datetime.date.today()}/data.parquet'
+
     try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print(f"Upload Successful: {s3_file}")
+        s3.upload_file(local_file, bucket, s3_file_path)
+        print(f"Upload Successful: {s3_file_path}")
         return True
     except FileNotFoundError:
         print("The file was not found")
